52eb97f17ca17e4317b6a7553b4c2e73f9da98a3
Wang <415892223@qq.com>
2019-11-21 22:40:38 +0800

    初始化设置

--- guest/settings.py
+++ guest/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'NAME': 'guest',
+        'NAME': 'guest_test',
         'USER': 'root',
         'PASSWORD': 'root',
         'OPTIONS': {
