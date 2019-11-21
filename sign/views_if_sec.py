398dd671d12a0e9085eb4a8c258fcce2fcc20133
Wang <415892223@qq.com>
2019-11-21 22:39:43 +0800

    使用Django接口开发带Auth接口

---
+++ sign/views_if_sec.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/21 22:36
+# @Author  : Fei.Wang
+# @Email   : 415892223@qq.com
+# @File    : views_if_sec.py
+# @Software: PyCharm
+
+from django.contrib import auth as django_auth
+import base64
+
+
+'''用户认证'''
+def user_auth(request):
